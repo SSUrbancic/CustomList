96b3053273c52de353a2a558e16a2150978c5d83
Sam <SSUrbancic@outlook.com>
2018-07-06 15:09:07 -0500

    Created a generic array. Also, created variables for count and capacity. Using the get accessor for both count and capacity. In the constructor I declared a starting capacity of 5 and set the count equal to 0. Also instantiated a generic array using the capacity variable to set the arrays capacity to 5. Wrote out an indexer using a generic(T) value to allow an instance to add elements to like an array. Created a function called Add() with logic set in place to iterate through the array and add specified values to each individual index. Add() function passed a test that pushed one item to index 0. Need to ensure that the logic takes into account when multiple elements are pushed to the list so that it will resize accordingly rather than generating an error.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        //Members(HAS A)
+
+        // generic aray
+        public T[] myArray;
+
+        private int count;
+        private int capacity;
+        
+        //Constructor
+
+        public virtual int Capacity { get; }
+        public virtual int Count { get; }
+        public CustomList()
+        {
+            capacity = 5;
+            myArray = new T[capacity];
+            count = 0;
+        }
+        //Methods(CAN DO)
+        public T this[int i]
+        {
+            get { return myArray[i]; }
+            set { myArray[i] = value; }
+        }
+        public void Add(T value)
+        {
+            
+            if(myArray.Length + 5 >= capacity)
+            {
+                capacity = capacity * 2;
+            }
+            for(count = 0; count < myArray.Length; count++)
+            {
+                myArray[count] = value;
+            }
+        }
+    }
+}
